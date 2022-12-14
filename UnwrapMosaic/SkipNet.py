e46de9903554ea6d1d6c62c4c27d3b85db72ee8d
Zehua Zhu <blackpatton17@gmail.com>
2022-12-14 17:43:30 -0500

    Revert "make it fit to py38"
    This reverts commit f2541f1e88292593a132f9e9c42e07265082d94e.

--- UnwrapMosaic/SkipNet.py
+++ UnwrapMosaic/SkipNet.py
@@ -61,7 +61,7 @@ def weights_init_normal(m):
     elif classname.find('Linear') != -1:
         init.uniform(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm2d') != -1:
-        init.uniform(m.weight.data, 0.02, 1.0)
+        init.uniform(m.weight.data, 1.0, 0.02)
         init.constant(m.bias.data, 0.0)
 
 
@@ -85,7 +85,7 @@ def weights_init_kaiming(m):
     elif classname.find('Linear') != -1:
         init.kaiming_normal(m.weight.data, a=0, mode='fan_in')
     elif classname.find('BatchNorm2d') != -1:
-        init.uniform(m.weight.data, 0.02, 1.0)
+        init.uniform(m.weight.data, 1.0, 0.02)
         init.constant(m.bias.data, 0.0)
 
 
@@ -97,7 +97,7 @@ def weights_init_orthogonal(m):
     elif classname.find('Linear') != -1:
         init.orthogonal(m.weight.data, gain=1)
     elif classname.find('BatchNorm2d') != -1:
-        init.uniform(m.weight.data, 0.02, 1.0)
+        init.uniform(m.weight.data, 1.0, 0.02)
         init.constant(m.bias.data, 0.0)
 
 
@@ -289,7 +289,7 @@ class UnetSkipConnectionBlockBetterUpsampler(nn.Module):
         upnorm = norm_layer(outer_nc)
 
         if outermost:
-            upsample = nn.Upsample(Resize_factor=2, mode='bilinear',align_corners=True)
+            upsample = nn.Upsample(scale_factor=2, mode='bilinear',align_corners=True)
             upconv = nn.Conv2d(inner_nc * 2, outer_nc,
                                         kernel_size=3, stride=1,
                                         padding=1, bias=use_bias)
@@ -298,7 +298,7 @@ class UnetSkipConnectionBlockBetterUpsampler(nn.Module):
             self.up = nn.Sequential(*up)
             self.down = nn.Sequential(*down)
         elif innermost:
-            upsample = nn.Upsample(Resize_factor=2, mode='bilinear',align_corners=True)
+            upsample = nn.Upsample(scale_factor=2, mode='bilinear',align_corners=True)
             upconv = nn.Conv2d(inner_nc, outer_nc,
                                         kernel_size=3, stride=1,
                                         padding=1, bias=use_bias)
@@ -307,7 +307,7 @@ class UnetSkipConnectionBlockBetterUpsampler(nn.Module):
             self.up = nn.Sequential(*up)
             self.down = nn.Sequential(*down)
         else:
-            upsample = nn.Upsample(Resize_factor=2, mode='bilinear',align_corners=True)
+            upsample = nn.Upsample(scale_factor=2, mode='bilinear',align_corners=True)
             upconv = nn.Conv2d(inner_nc * 2, outer_nc,
                                         kernel_size=3, stride=1,
                                         padding=1, bias=use_bias)
